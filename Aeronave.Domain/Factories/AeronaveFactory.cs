9426ab6c1ae6f4641b708987b5621a2b50a9e201
EuchiMano <marce.ps666@gmail.com>
2022-07-23 09:17:54 -0400

    linter prueba

--- Aeronave.Domain/Factories/AeronaveFactory.cs
+++ Aeronave.Domain/Factories/AeronaveFactory.cs
@@ -6,8 +6,8 @@ namespace Aeronave.Domain.Factories
     public class AeronaveFactory : IAeronaveFactory
     {
         public AeronaveModel Create(string marca, string modelo, int capacidad, int nroAsientos, int capacidadTanque, Aeropuerto aeropuerto)
-        {
+        
             return new AeronaveModel(marca, modelo, capacidad, nroAsientos, capacidadTanque, aeropuerto);
-        }
     }
+}
 }
